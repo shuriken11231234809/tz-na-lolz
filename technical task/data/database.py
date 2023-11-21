4d4884790cc7a3f5ba6fb69081082195edd4c7eb
shuriken11231234809 <151663657+shuriken11231234809@users.noreply.github.com>
2023-11-21 20:13:23 +0300

    Update database.py

--- technical task/data/database.py
+++ technical task/data/database.py
@@ -2,14 +2,14 @@ import sqlite3
 
 
 class Database:
-	def __init__(self, db_name):
+	def __init__(self, db_name): # инициализация бд с созданием таблиц
 		self.conn = sqlite3.connect(db_name)
 		self.cursor = self.conn.cursor()
 		self.create_main_table()
 
 
 
-	def create_main_table(self):
+	def create_main_table(self): # создание таблицы
 		self.cursor.execute(
 			'''CREATE TABLE IF NOT EXISTS books (
 							id INTEGER,
@@ -24,30 +24,24 @@ class Database:
 
 
 
-	def add(self, id, title, author, description, genre):
+	def add(self, id, title, author, description, genre): # добавление книги
 		self.cursor.execute('INSERT INTO books (id, title, author, description, genre) VALUES (?, ?, ?, ?, ?)', (id, title, author, description, genre))
 		self.conn.commit()
 
 
 
-	def add_genre(self, id, genre):
-		self.cursor.execute('INSERT INTO book_genres (id, genre) VALUES (?, ?)', (id, genre))
-		self.conn.commit()
-
-
-
-	def delete(self, id):
+	def delete(self, id): # удаление книги
 		self.cursor.execute('DELETE FROM books WHERE id = ?', (id,))
 		self.conn.commit()
 
 
 
-	def search(self, word):
+	def search(self, word): # поиск книги по ID, либо по жанру
 		return self.cursor.execute('SELECT * FROM books WHERE ID = ? OR genre LIKE ?', (word, '%' + word + '%')).fetchall()
 
 
 
-	def get_all(self):
+	def get_all(self): # получение всех книг
 		return self.cursor.execute('SELECT * FROM books').fetchall()
 
 
